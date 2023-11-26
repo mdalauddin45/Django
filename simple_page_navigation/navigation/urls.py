9535e1e6b76ca60a079bac1218de82bd5cdb5047
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-26 23:34:34 +0600

    contact done

--- simple_page_navigation/navigation/urls.py
+++ simple_page_navigation/navigation/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path,include
-
+from .import views
 urlpatterns = [
+    path('about/',views.about),
+    path('contact/',views.contact),
 ]
