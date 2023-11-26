ff704d97ab73b217783bd0d9254cb8d12d55c42b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-26 22:51:53 +0600

    project two

--- project_2/first_app/views.py
+++ project_2/first_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def Home(request):
-    return HttpResponse("this is my home page")
+def home(request):
+    return render(request,'first_app/home.html')
