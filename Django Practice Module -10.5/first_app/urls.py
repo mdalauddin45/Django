36d9d63f76aef3557f88907c4bf25577386c0cf1
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-28 00:27:14 +0600

    earthlyblog Template Filters

--- Django Practice Module -10.5/first_app/urls.py
+++ Django Practice Module -10.5/first_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('blog/',views.earthlyblog)
 ]
--- Django Practice Module -10.5/first_app/views.py
+++ Django Practice Module -10.5/first_app/views.py
@@ -50,3 +50,55 @@ def home(request):
         ]
     }
     return render(request,'home.html',d)
+
+def earthlyblog(request):
+    blog = {
+        'author': 'rahim', 'age': 10,'address': 'i’m Jai',
+        'list': ['a', 'b', 'c'],
+        'lst': ['python', 'is', 'fun'],
+        'lists': [1,2,3],
+        'date': datetime.datetime.now(),
+        'empty': "the empty string",
+        'string': 'String with spaces',
+        'time': datetime.datetime.now(),
+        'course': 'django',
+        'published':'10 AM on 1 January 2023',
+        'joe': 'Joseph is a developer',
+        'blog_date': '1 June 2006',
+        'comment_date': '08:00 on 1 June 2006',
+        'divisibleby': 21,
+        'size': 123456789,
+        'name': 'My Name is Jai',
+        'title': 'my FIRST post',
+        'hello': 'Jai is a slug',
+        'number': [
+            'one',
+            'two',
+            'three',
+        ],
+        'dictsort':[
+            {'name': 'zed', 'age': 19},
+            {'name': 'amy', 'age': 22},
+            {'name': 'joe', 'age': 31},
+                    ],
+        'courses': [
+            {
+                'id': 1,
+                'name': "python",
+                'fee': 5000
+            },
+            {
+                'id': 2,
+                'name': "C++",
+                'fee': 500
+            },
+            {
+                'id': 3,
+                'name': "django",
+                'fee': 2500
+            },
+            
+            
+        ]
+    }
+    return render(request,'earthlyblog.html',blog)
