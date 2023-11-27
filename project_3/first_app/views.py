d9b90794494bea025580d3dee11fb88786204137
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-27 12:07:35 +0600

    for loop

--- project_3/first_app/views.py
+++ project_3/first_app/views.py
@@ -3,4 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-     return render(request, 'home.html')
+    d = {
+        'author': 'Rahim', 'age': 5,
+        'list': [ 1,32,3,4,5,6,7,8,9,10,11,12,13,14],
+        'courses': [
+            {
+                'id': 1,
+                'name': "python",
+                'fee': 5000
+            },
+            {
+                'id': 2,
+                'name': "C++",
+                'fee': 500
+            },
+            {
+                'id': 3,
+                'name': "django",
+                'fee': 2500
+            },
+            
+            
+        ]
+    }
+    return render(request, 'home.html',d)
