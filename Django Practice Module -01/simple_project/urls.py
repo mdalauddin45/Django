f467a8e49970955e1433a4ccc487b8fddd17880c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-27 22:53:02 +0600

    practice module add

--- Django Practice Module -01/simple_project/urls.py
+++ Django Practice Module -01/simple_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app',include('first_app.urls'))
+    path('first_app/',include('first_app.urls'))
 ]
