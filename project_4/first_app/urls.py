6c48d8fafc8c72d045d43d2b08137b960a9154a8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-29 00:50:40 +0600

    urls

--- project_4/first_app/urls.py
+++ project_4/first_app/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path,include
 from .import views
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name='home'),
+    path('about/',views.index, name='about'),
 ]
