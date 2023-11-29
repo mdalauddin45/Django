7cb751fab0ab0464060dcda1c3a3d511edaa4e55
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-30 01:49:44 +0600

    home page add

--- Django Practice Module -11.5/Meal/first_app/views.py
+++ Django Practice Module -11.5/Meal/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
