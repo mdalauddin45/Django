202c3e188b46c4d93a3c50592029042393887afc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-03 10:25:42 +0600

    timefield

--- Module 13/first_project/first_app/forms.py
+++ Module 13/first_project/first_app/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label="username")
     email = forms.EmailField(label="Email address")
+    age = forms.IntegerField(label="Age")
+    weight = forms.FloatField(label="Weight")
+    balance = forms.DecimalField(label="Balance")
+    checked = forms.BooleanField(label="Check")
+    birthday = forms.DateField(label="Birthday")
+    appointment = forms.DateTimeField(label="Appointment")
