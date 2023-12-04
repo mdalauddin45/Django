26d848fc834951529441d69107b3fe6ed46249f7
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-05 00:24:35 +0600

    contact form done

--- Django Practice Module -14.5/practice/first_app/forms.py
+++ Django Practice Module -14.5/practice/first_app/forms.py
@@ -1,5 +1,24 @@
+from typing import Any
 from django import forms 
+import datetime
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
+    title = forms.CharField() 
+    description = forms.CharField()  
+    first_name = forms.CharField(max_length = 200, initial="Ala")  
+    last_name = forms.CharField(max_length = 200, initial="Uddin")  
+    roll_number = forms.IntegerField(help_text = "Enter 6 digit roll number")  
+    password = forms.CharField(widget = forms.PasswordInput()) 
+    comment = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField()
+    agree = forms.BooleanField()
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    BIRTH_YEAR_CHOICES = ['1980', '1981', '1982']
+    birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    message = forms.CharField(max_length = 10,help_text="Enter only 10 characters")   
+    day = forms.DateField(initial=datetime.date.today)
+    Favorite_color_choices =[('blue','Blue'),('green','Green'),('yellow','yellow')]
+    favorite_color = forms.ChoiceField(choices=Favorite_color_choices)
+    favorite_colors = forms.MultipleChoiceField(choices=Favorite_color_choices,widget=forms.CheckboxSelectMultiple)
+
     
--- Django Practice Module -14.5/practice/first_app/views.py
+++ Django Practice Module -14.5/practice/first_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return render(request, 'home.html',{'form':form})
+    else:
+        form = ContactForm()
+        return render(request, 'home.html',{'form':form})
