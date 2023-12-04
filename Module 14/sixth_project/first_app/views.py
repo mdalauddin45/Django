34dcdf6889e700ea03b655c710a72d0bc5183162
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-04 16:43:04 +0600

    delete successfuly

--- Module 14/sixth_project/first_app/views.py
+++ Module 14/sixth_project/first_app/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from . import models
 # Create your views here.
 
 def home(request):
     student = models.Student.objects.all()
-    print(student)
     return  render(request, 'home.html', {'data': student})
 
 def delete_student(request,roll):
-    std = models.Student.objects.get(pk = roll)
-    print(std)
+    std = models.Student.objects.get(pk = roll).delete()
+    student = models.Student.objects.all()
+    return redirect("home")
