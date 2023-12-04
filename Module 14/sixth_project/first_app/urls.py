d83cdfc71e520685276c73d472fe3721d33c0bc5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-04 16:32:19 +0600

    url

--- Module 14/sixth_project/first_app/urls.py
+++ Module 14/sixth_project/first_app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from .import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('delete/<int:roll>',views.delete_student,name='delete_student'),
 ]
--- Module 14/sixth_project/first_app/views.py
+++ Module 14/sixth_project/first_app/views.py
@@ -6,3 +6,7 @@ def home(request):
     student = models.Student.objects.all()
     print(student)
     return  render(request, 'home.html', {'data': student})
+
+def delete_student(request,roll):
+    std = models.Student.objects.get(pk = roll)
+    print(std)
