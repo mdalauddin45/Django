d2b7943f795f27a179147071432090fca30909a2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-04 12:38:52 +0600

    class add

--- Module 14/sixth_project/first_app/models.py
+++ Module 14/sixth_project/first_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length= 255)
+    roll = models.IntegerField(primary_key=True)
+    address = models.TextField()
+    father_name = models.TextField(default='Rahim')
