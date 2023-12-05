9853fae8e8ee2f70fd482cb793f5023136072b66
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-05 22:09:04 +0600

    forms done

--- Blog_Project/author/views.py
+++ Blog_Project/author/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .import forms
 # Create your views here.
 
 def add_author(request):
-    return render(request, 'author.html')
+    author_form = forms.AuthorForm()
+    return render(request, 'author.html',{'form':author_form})
