dea220fd7525dc461673ed022b2b02be584a499c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-05 22:36:36 +0600

    add profile page done

--- Blog_Project/author/views.py
+++ Blog_Project/author/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .import forms
 # Create your views here.
 
 def add_author(request):
-    author_form = forms.AuthorForm()
+    if request.method == 'POST':
+        author_form = forms.AuthorForm(request.POST)
+        if author_form.is_valid():
+            print(author_form.cleaned_data)
+            author_form.save()
+            return redirect('add_author')
+    else:
+        author_form = forms.AuthorForm()
     return render(request, 'author.html',{'form':author_form})
