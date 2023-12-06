4c7821e13184a7251b5f8bdb994bc113d53ad1a2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-06 23:07:25 +0600

    models add

--- Django Practice Module -15.5/Musicians_Directory/Album/models.py
+++ Django Practice Module -15.5/Musicians_Directory/Album/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from musician.models import Musician
 
 # Create your models here.
+class Album(models.Model):
+    album_name  = models.CharField(max_length=225)
+    musicians = models.ManyToManyField(Musician)
+    release_date  = models.DateField()
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    
