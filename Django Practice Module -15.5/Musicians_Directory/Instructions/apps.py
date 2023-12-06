5fd7352fd8e5c1b89a5dec01c3aadf1dab6e4c24
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-06 22:56:27 +0600

    show in home album and musician page

--- Django Practice Module -15.5/Musicians_Directory/Instructions/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstructionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Instructions'
--- Django Practice Module -15.5/Musicians_Directory/musician/views.py
+++ Django Practice Module -15.5/Musicians_Directory/musician/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def musician(request):
+    return render(request, 'musician.html')
