5ee73e1e76487baa34133bdf4beeae11ec912f38
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-10 12:44:15 +0600

    form

--- Module 17/auth_project/first_app/views.py
+++ Module 17/auth_project/first_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Account Created Successfully')
+            form.save()
+            print(form.cleaned_data)
+    else:
+        form = RegisterForm()
+    return render(request,'home.html',{'form':form})
