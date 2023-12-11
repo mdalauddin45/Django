b45aed77e74a2ef1b388a76daee3962016399e75
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-11 23:18:31 +0600

    registration form done

---
+++ Django Practice Module -18.5/Authentication/first_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    
+    class  Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
--- Django Practice Module -18.5/Authentication/first_app/views.py
+++ Django Practice Module -18.5/Authentication/first_app/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .import forms
+from django.contrib import messages
 # Create your views here.
 def profile(request):
     return render(request, 'profile.html')
 def registration(request):
-    return render(request, 'registration.html',{'type': 'registration'})
+    if request.method == 'POST':
+        form = forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            messages.success(request, "Account Created successfully")
+            return redirect('register')
+    else:
+        form = forms.RegistrationForm()
+    return render(request, 'registration.html',{ 'form': form, 'type': 'registration'})
 def userlogin(request):
     return render(request, 'registration.html',{'type': 'Login'})
 def userlogout(request):
