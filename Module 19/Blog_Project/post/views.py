3b5f2e452f64676eaf86b421b67b5c8d01867ce5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-12 23:31:26 +0600

    Comment

--- Module 19/Blog_Project/post/views.py
+++ Module 19/Blog_Project/post/views.py
@@ -71,18 +71,20 @@ class DetailsPostView(DetailView):
     pk_url_kwarg = 'id'
     template_name = 'post_details.html'
     
+    def post(self, request, *args, **kwargs):
+        comment_form = forms.CommentForm(data=self.request.POST)
+        post = self.get_object()
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         post = self.object #post model er object akany store korlam
-        comments = post.comments
-        if self.request.method == "POST":
-            comment_form = forms.CommentForm(data=self.request.post)
-            if comment_form.is_valid():
-                new_comment = comment_form.save(commit=False)
-                new_comment.post = post
-                new_comment.save()
-        else:
-            comment_form= forms.CommentForm()
+        comments = post.comments.all()
+        comment_form= forms.CommentForm()
             
         context['comments']= comments
         context['comment_form']= comment_form
