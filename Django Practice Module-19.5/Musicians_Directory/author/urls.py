a891e6640c1ef81dc2a2211109dbe91f8b76cc57
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-13 22:27:48 +0600

    UserLoginForm done

--- Django Practice Module-19.5/Musicians_Directory/author/urls.py
+++ Django Practice Module-19.5/Musicians_Directory/author/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('register/', views.registrationView.as_view(), name='register'),
+    path('login/', views.UserLoginForm.as_view(), name='login'),
 ]
--- Django Practice Module-19.5/Musicians_Directory/author/views.py
+++ Django Practice Module-19.5/Musicians_Directory/author/views.py
@@ -1,7 +1,9 @@
+from typing import Any
 from django.shortcuts import render,redirect
 from django.views import View
 from .import forms
 from django.contrib import messages
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView,LogoutView
 # Create your views here.
 class registrationView(View):
@@ -16,3 +18,19 @@ class registrationView(View):
             messages.success(request, "Account Created successfully")
             return redirect('login')
         return render(request,'authform.html',{'form': form, 'type': 'Registration'})
+
+class UserLoginForm(LoginView):
+    template_name = 'authform.html'
+    success_url= reverse_lazy('home')
+    
+    def form_valid(self, form):
+        messages.success(form, "Account Login successfully")
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.warning(form, "Login Failed")
+        return super().form_invalid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type']='Login'
+        return context
+    
