e561789f5661f2f691295ec97899f73f469a8c2b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-13 22:20:28 +0600

    registrationView

--- Django Practice Module-19.5/Musicians_Directory/author/views.py
+++ Django Practice Module-19.5/Musicians_Directory/author/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views import View
+from .import forms
+from django.contrib import messages
+from django.contrib.auth.views import LoginView,LogoutView
 # Create your views here.
+class registrationView(View):
+    def get(self,request):
+        form = forms.RegistrationForm()
+        return render(request,'authform.html',{'form': form, 'type': 'Registration'})
+    
+    def post(self,request):
+        form = forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account Created successfully")
+            return redirect('login')
+        return render(request,'authform.html',{'form': form, 'type': 'Registration'})
