3a28db19dc624f7e95ae7a6df1e45242f0ed6717
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-13 22:59:42 +0600

    UserLogoutView

--- Django Practice Module-19.5/Musicians_Directory/author/views.py
+++ Django Practice Module-19.5/Musicians_Directory/author/views.py
@@ -2,35 +2,51 @@ from typing import Any
 from django.shortcuts import render,redirect
 from django.views import View
 from .import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView,LogoutView
-# Create your views here.
-class registrationView(View):
-    def get(self,request):
-        form = forms.RegistrationForm()
-        return render(request,'authform.html',{'form': form, 'type': 'Registration'})
-    
-    def post(self,request):
-        form = forms.RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Account Created successfully")
-            return redirect('login')
-        return render(request,'authform.html',{'form': form, 'type': 'Registration'})
+from django.views.generic import CreateView
 
-class UserLoginForm(LoginView):
+# Create your views here.
+class registrationView(CreateView):
     template_name = 'authform.html'
-    success_url= reverse_lazy('home')
-    
+    form_class = forms.RegistrationForm
+    success_url = reverse_lazy('login')
+
     def form_valid(self, form):
-        messages.success(form, "Account Login successfully")
+        messages.success(self.request, "Account Created successfully")
         return super().form_valid(form)
+
     def form_invalid(self, form):
-        messages.warning(form, "Login Failed")
+        messages.warning(self.request, "Creation Failed")
         return super().form_invalid(form)
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['type']='Login'
+        context['type']='Registration'
         return context
-    
+class UserLoginForm(LoginView):
+    template_name = 'authform.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Account Login successfully")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.warning(self.request, "Login Failed")
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        messages.success(self.request, "Logout successfully")
+        return reverse_lazy('home')
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'profile.html')
