28d7f755dcc34c763510d8467f25ff6311a4ccdc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-12-16 12:56:49 +0600

    purchase quantity setting done

--- Car_Sales/card/urls.py
+++ Car_Sales/card/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns = [
     path('details/<int:id>',views.DetailsPostView.as_view(), name='details_post'),
-    path('purchase/<int:id>/', views.PurchaseCarView.as_view(), name='purchase_car'),
+    path('purchase/<int:id>/', views.purchase, name='purchase_car'),
 ]
--- Car_Sales/card/views.py
+++ Car_Sales/card/views.py
@@ -5,14 +5,13 @@ from .import models
 from django.views.generic import DetailView
 from .models import Card
 from django.shortcuts import get_object_or_404
-from django.views import View
 from django.contrib import messages
 from author.models import Purchase
-from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+
 class DetailsPostView(DetailView):
     model = models.Card
     pk_url_kwarg = 'id'
@@ -36,22 +35,21 @@ class DetailsPostView(DetailView):
         context['comments']= comments
         context['comment_form']= comment_form
         return context
-    
-@method_decorator(login_required, name='dispatch')
-class PurchaseCarView(View):
-    def get(self, request, id):
-        card = get_object_or_404(Card, id=id)
-        if card.quantity > 0:
+
+@login_required
+def purchase(request,id):
+    card = get_object_or_404(Card, id=id)
+    if card.quantity > 0:
             card.quantity -= 1
             card.save()
             purchase = Purchase.objects.create(user=request.user, car=card)
             
             messages.success(request, "Buy successfully")
             return redirect('profile')
-            
-        else:
+    else:
             messages.error(request, "Car out of stock!")
             return redirect('profile')
+        
 
 
            
