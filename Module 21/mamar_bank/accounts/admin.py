3c238d60e4cf6dc5a9704f99ca2ce0fec6e895db
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-23 23:53:13 +0600

    UserAdderss

--- Module 21/mamar_bank/accounts/admin.py
+++ Module 21/mamar_bank/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import UserBankAccount,UserAdderss
 # Register your models here.
+admin.site.register(UserBankAccount)
+admin.site.register(UserAdderss)
--- Module 21/mamar_bank/accounts/models.py
+++ Module 21/mamar_bank/accounts/models.py
@@ -7,10 +7,13 @@ class UserBankAccount(models.Model):
     user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE)
     account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPE)
     account_no = models.IntegerField(unique=True)
-    birth_date = models.DateTimeField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_TYPE)
     initial_deposit_date = models.DateField(auto_now_add=True)
     balance = models.DecimalField(default=0, decimal_places=2,max_digits=12)
+    
+    def __str__(self):
+        return str(self.account_no)
 
 class UserAdderss(models.Model):
     user = models.OneToOneField(User, related_name='address', on_delete=models.CASCADE)
@@ -18,3 +21,6 @@ class UserAdderss(models.Model):
     city = models.CharField(max_length=100)
     postal_code = models.IntegerField()
     country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return str(self.user)
