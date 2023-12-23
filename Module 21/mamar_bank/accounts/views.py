200fa69f69db17dd6c507babc5f62444a028ee7b
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-24 00:16:18 +0600

    UserRegistrationView

--- Module 21/mamar_bank/accounts/views.py
+++ Module 21/mamar_bank/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
 
 # Create your views here.
+class UserRegistrationView(FormView):
+    template_name = ''
+    form_class = UserRegistrationForm
+    success_url = ''
+    
+    def form_valid(self,form):
+        user = form.save()
+        login(user)
+        return super().form_valid(form)
