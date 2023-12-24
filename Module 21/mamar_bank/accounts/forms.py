fd7d24c1ad50fc47fa655dcfc399e4a0afeceb5a
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-24 23:19:43 +0600

    tailwind css

--- Module 21/mamar_bank/accounts/forms.py
+++ Module 21/mamar_bank/accounts/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django.contrib.auth.models import User
@@ -43,3 +44,15 @@ class UserRegistrationForm(UserCreationForm):
                 account_no=1212000600+our_user.id
             )
         return our_user
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class' : (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ) 
+            })
