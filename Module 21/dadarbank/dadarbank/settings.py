499e222eb183e56ba569d8ea9dd8b4677b647a1d
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-26 17:51:44 +0600

    done

--- Module 21/dadarbank/dadarbank/settings.py
+++ Module 21/dadarbank/dadarbank/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'core',
+    'accounts',
+    'transactions',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'dadarbank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Module 21/dadarbank/dadarbank/urls.py
+++ Module 21/dadarbank/dadarbank/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('transactions/', include('transactions.urls')),
 ]
