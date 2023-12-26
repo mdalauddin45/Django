7d44355e733e4e6861ab262d332300e4365b31e8
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-27 01:16:04 +0600

    deposit email

--- Module 21/dadarbank/dadarbank/settings.py
+++ Module 21/dadarbank/dadarbank/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = env("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rf-2v1@qje)l9s#diabj=(nz$z(sl$pe-%a=51qy-qo(m7r%x6'
+
+
+# SECRET_KEY = 'django-insecure-rf-2v1@qje)l9s#diabj=(nz$z(sl$pe-%a=51qy-qo(m7r%x6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +83,24 @@ WSGI_APPLICATION = 'dadarbank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': env("DB_NAME"),
+       'USER': env("DB_USER"),
+       'PASSWORD': env("DB_PASSWORD"),
+       'HOST': env("DB_HOST"),
+       'PORT': env("DB_PORT"),
+   }
 }
+
 
 
 # Password validation
@@ -125,3 +143,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
--- Module 21/dadarbank/transactions/views.py
+++ Module 21/dadarbank/transactions/views.py
@@ -15,6 +15,17 @@ from transactions.forms import (
     LoanRequestForm,
 )
 from transactions.models import Transaction
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_transaction_email(user,amount,subject,template):
+    message = render_to_string(template,{
+        'user': user,
+        'amount':amount,
+    })
+    send_email = EmailMultiAlternatives(subject, '',to=[user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
 
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = 'transactions/transaction_form.html'
@@ -63,7 +74,16 @@ class DepositMoneyView(TransactionCreateMixin):
             self.request,
             f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully'
         )
-
+        send_transaction_email(self.request.user,amount,"Deposit Message", 'transactions/deposit_email.html' )
+        # mail_subject = "Deposit Message"
+        # message = render_to_string('transactions/deposit_email.html',{
+        #     'user': self.request.user,
+        #     'amount':amount,
+        # })
+        # to_email= self.request.user.email
+        # send_email = EmailMultiAlternatives(mail_subject, '',to=[to_email])
+        # send_email.attach_alternative(message, "text/html")
+        # send_email.send()
         return super().form_valid(form)
 
 
@@ -87,7 +107,7 @@ class WithdrawMoneyView(TransactionCreateMixin):
             self.request,
             f'Successfully withdrawn {"{:,.2f}".format(float(amount))}$ from your account'
         )
-
+        send_transaction_email(self.request.user,amount,"Withdraw Message", 'transactions/deposit_email.html' )
         return super().form_valid(form)
 
 class LoanRequestView(TransactionCreateMixin):
@@ -108,7 +128,7 @@ class LoanRequestView(TransactionCreateMixin):
             self.request,
             f'Loan request for {"{:,.2f}".format(float(amount))}$ submitted successfully'
         )
-
+        send_transaction_email(self.request.user,amount,"Loan Request Message", 'transactions/deposit_email.html' )
         return super().form_valid(form)
     
 class TransactionReportView(LoginRequiredMixin, ListView):
