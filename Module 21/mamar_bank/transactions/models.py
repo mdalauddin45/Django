d0afee42eeb9700ab5fce3d674c859e4a6c79d15
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-26 13:32:24 +0600

    WithdrawForm

--- Module 21/mamar_bank/transactions/models.py
+++ Module 21/mamar_bank/transactions/models.py
@@ -7,7 +7,7 @@ class Transaction(models.Model):
     account = models.ForeignKey(UserBankAccount, related_name= 'transactions',on_delete=models.CASCADE)
     amount = models.DecimalField(decimal_places=2, max_digits=12)
     balance_after_transaction = models.DecimalField(decimal_places=2, max_digits=12)
-    Transaction_type = models.IntegerField(choices=TRANSACTION_TYPE, null=True)
+    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE, null=True)
     tiemstamp = models.DateTimeField(auto_now_add=True)
     loan_approve = models.BooleanField(default=False)
     
