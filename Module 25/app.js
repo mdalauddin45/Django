2755e4fbc8d15b102f2fdbb8c5189fd982bd3d79
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-31 21:21:02 +0600

    review done

--- Module 25/app.js
+++ Module 25/app.js
@@ -104,8 +104,32 @@ const handleSearch=()=>{
     const value = document.getElementById("search").value;
     loadDoctors(value)
 }
+const loadReview=()=>{
+    fetch("https://testing-8az5.onrender.com/doctor/review/")
+    .then((res)=>res.json())
+    .then((data)=>displayReview(data))
+    .catch((err)=>console.log(err));
+}
+const displayReview=(reviews)=>{
+    reviews.forEach((review)=>{
+        const parent = document.getElementById("all-review");
+        const div = document.createElement("div");
+        div.classList.add("review-card");
+        div.classList.add("slide-visible");
+        div.innerHTML = `
+            <img src="./Images/girl.png" alt="" />
+            <h4>${review.reviewer}</h4>
+            <p>
+             ${review.body.slice(0, 100)}
+            </p>
+            <h6>${review.rating}</h6>
+        `;
+        parent.appendChild(div);
+    });
+};
 
 loadServices();
 loadDoctors();
 loadDesignation();
 loadSpecialization();
+loadReview();
