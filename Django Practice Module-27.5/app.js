c95f2b25d34787c35db06b53f2d99a1bc3e9ac64
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-02 01:11:10 +0600

    done

--- Django Practice Module-27.5/app.js
+++ Django Practice Module-27.5/app.js
@@ -1,23 +1,38 @@
-const loadProducts=()=>{
-    fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(json=>displayProducts(json))
-}
-const displayProducts=(products)=>{
-    products.forEach(product=>{
-        const parent = document.getElementById("products");
-        console.log(product);
+const loadProducts = (cat) => {
+    document.getElementById("products").innerHTML = "";
+    console.log(cat);
+    fetch(`https://fakestoreapi.com/products/?category=${cat ? cat : ""}`)
+        .then(res => res.json())
+        .then((data) => {
+            if (data?.length > 0) {
+                document.getElementById("nodata").style.display = "none";
+                document.getElementById("spinner").style.display = "none";
+                displayProducts(data);
+            } else {
+                document.getElementById("nodata").style.display = "block";
+                document.getElementById("spinner").style.display = "none";
+            }
+        })
+        .catch((err) => console.log(err));
+};
+
+
+const displayProducts = (products) => {
+    const parent = document.getElementById("products");
+    parent.innerHTML = ""; 
+
+    products.forEach(product => {
         const div = document.createElement("div");
-        div.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-6")
-        div.classList.add("card-container")
-        div.innerHTML=`
+        div.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-6");
+        div.classList.add("card-container");
+        div.innerHTML = `
             <div class="card  text-center" style="width: 18rem;">
                 <img class="img-card" src="${product?.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${product?.title}</h5>
-                    <h5 >Price: ${product?.price}</h5>
+                    <h5>Price: ${product?.price}</h5>
                     <p class="card-text">Category: ${product?.category}</p>
-                    <p class="card-text">Ratting: ${product?.rating?.rate}</p>
+                    <p class="card-text">Rating: ${product?.rating?.rate}</p>
                     <a href="#" class="btn btn-primary">Details</a>
                 </div>
             </div>
@@ -25,20 +40,24 @@ const displayProducts=(products)=>{
         parent.appendChild(div);
     });
 };
-const loadCategories=()=>{
+
+const loadCategories = () => {
     fetch('https://fakestoreapi.com/products/categories')
-    .then(res=>res.json())
-    .then(json=>displayCategories(json))
-}
-const displayCategories=(categories)=>{
-    categories.forEach(category=>{
-        const parent = document.getElementById("category")
+        .then(res => res.json())
+        .then(json => displayCategories(json));
+};
+
+const displayCategories = (categories) => {
+    const parent = document.getElementById("category");
+    categories.forEach(category => {
         const buttonElement = document.createElement('button');
-        buttonElement.classList.add('btn', 'btn-primary','m-3');
-        buttonElement.textContent = category;
+        buttonElement.classList.add('btn', 'btn-primary', 'm-3');
+        buttonElement.innerHTML = `
+            <li onclick="loadProducts('${category}')">${category}</li>
+        `;
         parent.appendChild(buttonElement);
     });
-}
+};
 
 loadProducts();
 loadCategories();
