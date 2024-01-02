22f113c0abdb2d428cb6c8d797c5cf4baa1d3fc0
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-02 12:53:25 +0600

    available time

--- Module 25/docdetails.js
+++ Module 25/docdetails.js
@@ -1,5 +1,6 @@
 const getparams = () => {
   const param = new URLSearchParams(window.location.search).get("doctorId");
+  loadTime(param);
   fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
   .then((res) => res.json())
   .then((data) => displayDetails(data));
@@ -38,9 +39,24 @@ const displayDetails=(doctor)=>{
         ${ doctor?.specialization.map((s)=>`<button class="doc-detail-btn">${s}</button>`).join('')}
         ${ doctor?.designation.map((s)=>`<h4 >${s}</h4>`).join('')}
         <h4>Frees: ${doctor.fee} BDT</h4>
-        <button>Take Appointment</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Take Appointment</button>
       </div>
   `; 
   parent.appendChild(div);
 };
+const loadTime=(id)=>{
+  fetch(`https://testing-8az5.onrender.com/doctor/availabletime/?doctor_id=${id}`)
+  .then((res) => res.json())
+  .then((data) =>{
+    data.forEach((item)=>{
+      const parent = document.getElementById("time-container");
+      const option = document.createElement("option");
+      option.value = item.id;
+      option.innerText = item.name;
+      parent.appendChild(option);
+    });
+    console.log(data);
+  });
+}
+
 getparams();
