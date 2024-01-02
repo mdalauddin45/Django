a5abea5f3f177b6efa78b0baa99252cc33af6aaa
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-02 13:17:52 +0600

    appoinnment done

--- Module 25/docdetails.js
+++ Module 25/docdetails.js
@@ -55,8 +55,39 @@ const loadTime=(id)=>{
       option.innerText = item.name;
       parent.appendChild(option);
     });
-    console.log(data);
+    // console.log(data);
   });
 }
+const handleAppointment=()=>{
+  const param = new URLSearchParams(window.location.search).get("doctorId");
+  const status = document.getElementsByName("status");
+  const selected = Array.from(status).find((button) => button.checked);
+  const symptom = document.getElementById("symptom").value;
+  const time = document.getElementById("time-container");
+  const selectedTime = time.options[time.selectedIndex];
+  const patient_id = localStorage.getItem("patient_id");
+  const info = {
+    appointment_type: selected.value,
+    appointment_status: "Pending",
+    time: selectedTime.value,
+    symptom: symptom,
+    cancel: false,
+    patient: patient_id,
+    doctor: param,
+  };
+  // console.log(info);
 
+  fetch("https://testing-8az5.onrender.com/appointment/", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // window.location.href = `pdf.html?doctorId=${param}`;
+      // handlePdf();
+      console.log(data);
+    });
+};
 getparams();
+loadTime();
