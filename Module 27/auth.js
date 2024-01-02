97ec28da9f2008a58f8be1db802cf2ca7c9ffb5b
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-02 22:39:40 +0600

    pass valid

--- Module 27/auth.js
+++ Module 27/auth.js
@@ -1,21 +1,44 @@
 const handleRegistration =(event)=>{
     event.preventDefault();
     const username = getValue("username");
-    const firstname = getValue("first_name");
-    const lastname = getValue("last_name");
+    const first_name = getValue("first_name");
+    const last_name = getValue("last_name");
     const email = getValue("email");
-    const pass1 = getValue("password");
-    const pass2 = getValue("confirm_password");
+    const password = getValue("password");
+    const confirm_password = getValue("confirm_password");
     const info = {
-        username: username,
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        pass1: pass1,
-        pass2: pass2
+        username,
+        first_name,
+        last_name,
+        email,
+        password,
+        confirm_password
     };
-    console.log(info);
-
+    if (password === confirm_password) {
+        document.getElementById("error").innerText = "";
+        if (
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+            password
+          )
+        ) {
+          console.log(info);
+    
+          fetch("https://testing-8az5.onrender.com/patient/register/", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(info),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+        } else {
+          document.getElementById("error").innerText =
+            "pass must contain eight characters, at least one letter, one number and one special character:";
+        }
+      } else {
+        document.getElementById("error").innerText =
+          "password and confirm password do not match";
+        alert("password and confirm password do not match");
+      }
 }
 const getValue=(id)=>{
     const value = document.getElementById(id).value;
