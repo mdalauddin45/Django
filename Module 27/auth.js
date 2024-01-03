7c433da421b06275a908fc8622eebf59b6e3beb0
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-03 15:16:53 +0600

    user-details done

--- Module 27/auth.js
+++ Module 27/auth.js
@@ -43,4 +43,27 @@ const handleRegistration =(event)=>{
 const getValue=(id)=>{
     const value = document.getElementById(id).value;
     return value;
-};
+}
+const handleLogin = (event) => {
+    event.preventDefault();
+    const username = getValue("login-username");
+    const password = getValue("login-password");
+    console.log(username, password);
+    if ((username, password)) {
+      fetch("https://testing-8az5.onrender.com/patient/login/", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+  
+          if (data.token && data.user_id) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("user_id", data.user_id);
+            window.location.href = "index.html";
+          }
+        });
+    }
+  };
