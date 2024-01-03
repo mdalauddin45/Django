9ddc9ca6af6f0dc9645dbf7522f6a8f9af1c7f99
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-04 00:48:36 +0600

    search user done

--- Django Practice Module-27.5/allUser.js
+++ Django Practice Module-27.5/allUser.js
@@ -1,39 +1,52 @@
-const loadUser=()=>{
+const loadUser=(value)=>{
+    document.getElementById("alluser").innerHTML = "";
     document.getElementById("spinner").style.display = "block";
-    fetch(`https://fakestoreapi.com/users`)
+    fetch(`https://fakestoreapi.com/users/${value?  value: " "}`)
     .then((res)=>res.json())
     .then((data)=>{
+        console.log(data);
         document.getElementById("spinner").style.display = "none";
-        if (data?.length > 0) {
-            document.getElementById("nodata").style.display = "none";
-            displayUser(data);
-        } else {
+        if(Array.isArray(data)){
+            if (data?.length > 0 ) {
+                document.getElementById("nodata").style.display = "none";
+                displayUsers(data);
+            } else {
                 document.getElementById("nodata").style.display = "block";
             }
-        })
-        .catch((err) => console.log(err));
+        }else{
+            displayUser(data);
+        }
+    })
+    .catch((err) => console.log(err));
 };
-const displayUser=(users)=>{
-    users.forEach(user => {
-        const parent = document.getElementById("alluser");
-        const div = document.createElement("div");
-        div.classList.add("col-lg-3", "mb-lg-0", "col-md-6","col-12", "card-container");
-        div.innerHTML = `
-        <div class="card rounded shadow-sm border-0">
-            <div class="card-body p-4">
-                <p class="small text-muted font-italic">ID: ${user.id} </p>
-                <h5>${user.name.firstname} ${user.name.lastname} </h5>
-                <p>username: ${user.username} </p>
-                <p>phone: ${user.phone} </p>
-                <p>Email: ${user.email} </p>
-                <p>City: ${user.address.city} </p>
-                <p>Zip code: ${user.address.zipcode} </p>
-                <p>Street: ${user.address.street} </p>
-            </div>
-        </div>
-        `;
-        parent.appendChild(div);
-        console.log(user);
+const displayUsers=(users)=>{
+    users?.forEach(user => {
+        displayUser(user);
     });
+}
+const displayUser=(user)=>{
+    const parent = document.getElementById("alluser");
+    const div = document.createElement("div");
+    div.classList.add("col-lg-3", "mb-lg-0", "col-md-6","col-12", "card-container");
+    div.innerHTML = `
+    <div class="card rounded shadow-sm border-0">
+        <div class="card-body p-4">
+            <p class="small text-muted font-italic">ID: ${user.id} </p>
+            <h5>${user.name.firstname} ${user.name.lastname} </h5>
+            <p>username: ${user.username} </p>
+            <p>phone: ${user.phone} </p>
+            <p>Email: ${user.email} </p>
+            <p>City: ${user.address.city} </p>
+            <p>Zip code: ${user.address.zipcode} </p>
+            <p>Street: ${user.address.street} </p>
+        </div>
+    </div>
+    `;
+    parent.appendChild(div);
+}
+
+const handleSearch=()=>{
+    const value = document.getElementById("search").value;
+    loadUser(value);
 }
 loadUser();
