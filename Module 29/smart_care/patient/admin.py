cff283ab64ebcdf1b53da269ec9847beb8286131
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-08 15:29:55 +0600

    PatientAdmin

--- Module 29/smart_care/patient/admin.py
+++ Module 29/smart_care/patient/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Patient
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display=[ 'first_name','last_name' ,'mobile_no', 'image']
+    
+    def first_name(self,obj):
+        return obj.user.first_name
+    
+    def last_name(self,obj):
+        return obj.user.last_name
+    
+admin.site.register(Patient,PatientAdmin)
