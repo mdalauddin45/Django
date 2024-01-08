3ba904a92f884feffa97a0fbfb8cd68478ded351
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-08 15:12:21 +0600

    ContactModelAdmin

--- Module 29/smart_care/contact_us/admin.py
+++ Module 29/smart_care/contact_us/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ContactUs
 # Register your models here.
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ['id','name','phone','problem']
+admin.site.register(ContactUs, ContactModelAdmin)
