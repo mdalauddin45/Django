0d6a3dc38b0a7dcaec49e7159cf8521051adaba1
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-08 15:44:23 +0600

    Doctor

--- Module 29/smart_care/doctor/admin.py
+++ Module 29/smart_care/doctor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Doctor,Specialization,Designation,AvailableTime
 # Register your models here.
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',),}
+admin.site.register(Specialization,SpecializationAdmin)
+
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',),}
+admin.site.register(Designation,DesignationAdmin)
+
+admin.site.register(AvailableTime)
+admin.site.register(Doctor)
--- Module 29/smart_care/doctor/models.py
+++ Module 29/smart_care/doctor/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Specialization(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    
+    def __str__(self) :
+        return self.name
+    
+class Designation(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    
+    def __str__(self) :
+        return self.name
+    
+class AvailableTime(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) :
+        return self.name
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='doctor/images/')
+    designation = models.ManyToManyField(Designation)
+    specialization = models.ManyToManyField(Specialization)
+    availabletime = models.ForeignKey(AvailableTime, on_delete=models.CASCADE)
+    fee = models.IntegerField()
+    meetlink = models.CharField(max_length=100)
+    
+    def __str__(self) :
+        return f"{self.user.first_name} {self.user.last_name}"
