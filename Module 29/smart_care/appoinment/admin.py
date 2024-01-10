f5b0b9fba4df520026603887d61c132dac02531d
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-10 17:53:20 +0600

    module 29 done

--- Module 29/smart_care/appoinment/admin.py
+++ Module 29/smart_care/appoinment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Appointment
 # Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display=['doctor_name', 'patient_name',"appointment_types","appointment_status","symptom","time","cancel"]
+    
+    def patient_name(self,obj):
+        return obj.patient.user.first_name
+    
+    def doctor_name(self,obj):
+        return obj.doctor.user.last_name
+    
+admin.site.register(Appointment,AppointmentAdmin)
--- Module 29/smart_care/appoinment/models.py
+++ Module 29/smart_care/appoinment/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from patient.models import Patient
+from doctor.models import Doctor, AvailableTime
 
+
+APPOINTMENT_STATUS=[
+    ("Completed", "Completed"),
+    ("Pandding", "Pandding"),
+    ("Running", "Running"),
+]
+APPOINTMENT_TYPES=[
+    ("Online", "Online"),
+    ("Offline", "Offline"),
+]
 # Create your models here.
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE)
+    appointment_types = models.CharField(max_length=20, choices=APPOINTMENT_TYPES, default="Offline")
+    appointment_status = models.CharField(max_length=20, choices=APPOINTMENT_STATUS, default="Pandding")
+    symptom = models.TextField()
+    time = models.OneToOneField(AvailableTime, on_delete=models.CASCADE)
+    cancel = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Doctor : {self.doctor.user.first_name}, Patient : {self.patient.user.first_name}"
