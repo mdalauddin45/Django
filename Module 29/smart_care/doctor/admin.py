f8b1bb0480dcffb1208d1578c1604fb5c14ea299
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-10 17:25:24 +0600

    DesignationAdmin

--- Module 29/smart_care/doctor/admin.py
+++ Module 29/smart_care/doctor/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Doctor,Specialization,Designation,AvailableTime
+from .models import Doctor,Specialization,Designation,AvailableTime,Review
 # Register your models here.
 class SpecializationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',),}
@@ -11,3 +11,4 @@ admin.site.register(Designation,DesignationAdmin)
 
 admin.site.register(AvailableTime)
 admin.site.register(Doctor)
+admin.site.register(Review)
--- Module 29/smart_care/doctor/models.py
+++ Module 29/smart_care/doctor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from patient.models import Patient
 # Create your models here.
 class Specialization(models.Model):
     name = models.CharField(max_length=100)
@@ -32,3 +33,21 @@ class Doctor(models.Model):
     
     def __str__(self) :
         return f"{self.user.first_name} {self.user.last_name}"
+
+STAR_CHOICES = [
+    ('⭐', '⭐'),
+    ('⭐⭐', '⭐⭐'),
+    ('⭐⭐⭐', '⭐⭐⭐'),
+    ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
+    ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
+]
+class Review(models.Model):
+    reviewer = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    rating = models.CharField(choices= STAR_CHOICES,max_length=10)
+    
+    def __str__(self):
+        return f"Patient : {self.reviewer.user.first_name}; Doctor : {self.doctor.user.first_name}"
+    
