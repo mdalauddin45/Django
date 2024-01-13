d77fcb83232d772a0b42b9bd813f4307609c1e55
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-13 15:39:54 +0600

    all done

--- Module 29/smart_care/doctor/views.py
+++ Module 29/smart_care/doctor/views.py
@@ -3,17 +3,8 @@ from rest_framework import viewsets
 from .models import Doctor,Specialization, Designation,AvailableTime,Review
 from .serializers import DoctorSerializer,SpecializationSerializer,DesignationSerializer,AvailableTimeSerializer,ReviewSerializer
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import pagination, filters
 
-class DoctorViewset(viewsets.ModelViewSet):
-    queryset = Doctor.objects.all()
-    serializer_class = DoctorSerializer
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        user_id = self.request.query_params.get('user_id')
-        if user_id:
-            queryset = queryset.filter(user_id=user_id)
-        return queryset
     
 class SpecializationViewset(viewsets.ModelViewSet):
     queryset = Specialization.objects.all()
@@ -23,13 +14,35 @@ class DesignationViewset(viewsets.ModelViewSet):
     queryset = Designation.objects.all()
     serializer_class = DesignationSerializer
     
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, query_set, view):
+        doctor_id = request.query_params.get("doctor_id")
+        if doctor_id:
+            return query_set.filter(doctor = doctor_id)
+        return query_set
+
 class AvailableTimeViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = AvailableTime.objects.all()
     serializer_class = AvailableTimeSerializer
+    filter_backends = [AvailableTimeForSpecificDoctor]
+
+
+class DoctorPagination(pagination.PageNumberPagination):
+       page_size = 1 #items per page
+       page_size_query_param = page_size
+       max_page_size = 100
+       
+class DoctorViewset(viewsets.ModelViewSet):
+    queryset =Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    filter_backends = [filters.SearchFilter]
+    pagination_class = DoctorPagination
+    search_fields = ['user__first_name', 'user__email', 'designation__name', 'specialization__name']
     
 class ReviewViewset(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+    
+    queryset =Review.objects.all()
+    serializer_class =ReviewSerializer
     
     
